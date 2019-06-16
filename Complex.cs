1a2b4b4991ee33e52b07b1f8871307b556c62d48
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-06-17 11:38:16 +1200

    Add project files.

---
+++ Complex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace internalAccessModifier
+{
+    // Declare class Complex as internal 
+    internal class Complex
+    {
+        int real;
+        int img;
+
+        public void setData(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Real = {0}", real);
+            Console.WriteLine("Imaginary = {0}", img);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//In the code given below, The class Complex is a part of internalAccessModifier namespace and is accessible throughout it.
+// C# Program to show use of 
+// internal access modifier 
+// Inside the file Program.cs 
+
+namespace internalAccessModifier
+{
+    // Driver Class
+    static class Program
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+            // Instantiate the class Complex 
+            // in separate class but within  
+            // the same assembly 
+            Complex c = new Complex();
+
+            // Accessible in class Program 
+            c.setData(2, 1);
+            c.displayData();
+        }
+    }
+}
